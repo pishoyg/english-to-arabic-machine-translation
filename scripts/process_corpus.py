819282697ee65fff80c42ce8a9c45b7b2a489bbf
bishoyboshra <bishoyboshra95@gmail.com>
2019-02-07 15:54:08 +0200

    More edits to scripts

--- scripts/process_corpus.py
+++ scripts/process_corpus.py
@@ -6,7 +6,7 @@ from itertools import chain
 parser = argparse.ArgumentParser(description=
   'Read corpora, and generate'
   ' (1) A clean version of it.'
-  ' (2) Vocabulary file.'
+  ' (2) Vocabulary file, sorted by frequency.'
   ' (3) Alphabet file.'
   'Notice that for every language suffix specified in args.langs, '
   'a corresponding cleaner must be available from lang_to_cleaner.py.'
@@ -37,12 +37,23 @@ for lang in args.langs:
     corpus = input_file.read().split('\n')
 
   # Extract Info.
-  clean = [' '.join([cleaner.clean(word) for word in sentence.split()]) for sentence in corpus]
-
-  vocab = sorted(list(set(chain.from_iterable([sentence.split() for sentence in clean]))))
-
-  alphabet = sorted(list(set(chain.from_iterable([set(word) for word in vocab]))))
-
+  vocab = set()
+  clean = []
+  vocab_freq = dict()
+  print('reading corpus')
+  for sentence in corpus:
+    words = [cleaner.clean(word) for word in sentence.split()]
+    clean.append(' '.join(words))
+    for word in words:
+      if word not in vocab:
+        vocab.add(word)
+        vocab_freq[word] = 0
+      vocab_freq[word] += 1
+  print('sorting vocab by frequency')
+  vocab = sorted(list(vocab), key=lambda word: vocab_freq[word])
+  print('listing alphabet')
+  alphabet = sorted(list(set(c for c in word for word in vocab)), reverse=True)
+  print('writing info')
   # Write info.
   for name, info in [('alphabet', alphabet), ('vocab', vocab), ('clean', clean)]:
     with open('.'.join([args.input, name, lang]), 'w') as output_file:
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 DATA_PREFIX="/home/bishoyboshra/corpora/CorporaDec07/LDC2007T08/isi_ara_eng_parallel_corpus/data/ISI_ara_eng_parallel_corpus"
-MODEL_DIR="/${HOME}/models/nmt_model_${RANDOM}"
-NUM_LAYERS=3
-NUM_UNITS=300
+MODEL_DIR="${HOME}/models/nmt_model_${RANDOM}"
+NUM_LAYERS=2
+NUM_UNITS=128
 DROPOUT=0.2
 
 set -o xtrace
@@ -10,9 +10,9 @@ mkdir -p ${MODEL_DIR}
 python -m nmt.nmt.nmt \
     --src="eng" --tgt="ara" \
     --vocab_prefix="${DATA_PREFIX}.vocab" \
-    --train_prefix="${DATA_PREFIX}.clean_train" \
-    --dev_prefix="${DATA_PREFIX}.clean_dev" \
-    --test_prefix="${DATA_PREFIX}.clean_test" \
+    --train_prefix="${DATA_PREFIX}.clean_train_head" \
+    --dev_prefix="${DATA_PREFIX}.clean_dev_head" \
+    --test_prefix="${DATA_PREFIX}.clean_test_head" \
     --out_dir="${MODEL_DIR}" \
     --num_train_steps=12000000 \
     --steps_per_stats=100 \
