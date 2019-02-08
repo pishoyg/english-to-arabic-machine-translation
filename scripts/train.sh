5171b49dbc8f2df990da05364561bbdd27d593fd
bishoyboshra <bishoyboshra95@gmail.com>
2019-02-08 13:51:26 +0200

    Updating scripts.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,22 +1,64 @@
+# Data parameters.
 DATA_PREFIX="$1"
-MODEL_DIR="${HOME}/models/nmt_model_${RANDOM}"
-NUM_LAYERS=3
-NUM_UNITS=300
+SRC_V=40000
+TGT_V=40000
+SRC="eng"
+TGT="ara"
+# Model parameters.
+NUM_UNITS=2
+NUM_LAYERS=128
 DROPOUT=0.2
+BEAM_WIDTH=3
+# Stats parameters.
+NUM_TRAIN_STEPS=12000000
+STEPS_PER_STATS=100
+METRICS="bleu"
 
+OUT_DIR="${HOME}/models/${SRC}-${SRC_V}_${TGT}-${TGT_V}_${NUM_UNITS}x${NUM_LAYERS}_${DROPOUT}_${BEAM_WIDTH}"
+
+# Validate some arguments are present!
+for ARGUMENT in "${DATA_PREFIX}" "${NUM_LAYERS}" "${NUM_UNITS}"; do
+  if [[ "${ARGUMENT}" == "" ]]; then
+    echo 'Missing command line arguments!!' && exit 1
+  fi
+done
+
+# Because I use Python 3 on machines where my username is 'bishoy',
+# and Python 2.7 on machines where my username is 'bishoyboshra'!
+if [[ ${USER} == "bishoy" ]]; then
+  THREE="3"
+elif [[ ${USER} == "bishoyboshra" ]]; then
+  THREE=""
+fi
+
+# Make operations visible to user.
 set -o xtrace
-mkdir -p ${MODEL_DIR}
 
-python3 -m nmt.nmt.nmt \
-    --src="eng" --tgt="ara" \
-    --vocab_prefix="${DATA_PREFIX}.vocab.head" \
-    --train_prefix="${DATA_PREFIX}.clean.train.head" \
-    --dev_prefix="${DATA_PREFIX}.clean.dev.head" \
-    --test_prefix="${DATA_PREFIX}.clean.test.head" \
-    --out_dir="${MODEL_DIR}" \
-    --num_train_steps=12000000 \
-    --steps_per_stats=100 \
+# Set up directory and datasets.
+mkdir -p ${OUT_DIR}
+
+VOCAB_PREFIX="${OUT_DIR}/$(basename ${DATA_PREFIX}).vocab-head"
+PARTITION_PREFIX="${OUT_DIR}/$(basename ${DATA_PREFIX}).clean"
+head -${SRC_V} ${DATA_PREFIX}.vocab.${SRC} > ${VOCAB_PREFIX}.${SRC}
+head -${TGT_V} ${DATA_PREFIX}.vocab.${TGT} > ${VOCAB_PREFIX}.${TGT}
+for LANGUAGE in "${SRC}" "${TGT}"; do
+  for PARTITION in train dev test; do
+    cp ${DATA_PREFIX}.clean.${PARTITION}.${LANGUAGE} ${OUT_DIR}
+  done
+done
+
+# Start training.
+python${THREE} -m nmt.nmt.nmt \
+    --src="${SRC}" --tgt="${TGT}" \
+    --vocab_prefix="${VOCAB_PREFIX}" \
+    --train_prefix="${PARTITION_PREFIX}.train" \
+    --dev_prefix="${PARTITION_PREFIX}.dev" \
+    --test_prefix="${PARTITION_PREFIX}.test" \
+    --out_dir="${OUT_DIR}" \
+    --num_train_steps="${NUM_TRAIN_STEPS}" \
+    --steps_per_stats="${STEPS_PER_STATS}" \
     --num_layers="${NUM_LAYERS}" \
     --num_units="${NUM_UNITS}" \
     --dropout="${DROPOUT}" \
-    --metrics=bleu
+    --metrics="${METRICS}" \
+    --beam_width="${BEAM_WIDTH}"
