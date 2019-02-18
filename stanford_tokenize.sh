dfd1312d8ad6e74f0ffc2f22e581342be24d4f14
bishoyboshra <bishoyboshra95@gmail.com>
2019-02-18 22:39:30 +0200

    Updating stanford_tokenize.sh.

--- stanford_tokenize.sh
+++ stanford_tokenize.sh
@@ -2,6 +2,8 @@
 INSTALL_DIR="stanford-corenlp-full-2018-10-05"
 MODEL="stanford-arabic-corenlp-2018-10-05-models/edu/stanford/nlp/models/segmenter/arabic/arabic-segmenter-atb+bn+arztrain.ser.gz"
 INPUT_PATH=""
+LANGUAGE="ara"
+WORK_DIR="/tmp/tmp"
 EXTENSION="stanford"
 
 # Parsing flags.
@@ -15,6 +17,12 @@ while [ $# -gt 0 ]; do
       ;;
     --input_path=*)
       INPUT_PATH="${1#*=}"
+      ;;
+    --language=*)
+      LANGUAGE="${1#*=}"
+      ;;
+    --work_dir=*)
+      WORK_DIR="${1#*=}"
       ;;
     --extension=*)
       EXTENSION="${1#*=}"
@@ -39,27 +47,46 @@ done
 cd
 
 # Input validation.
-if [[ -z ${INPUT_PATH} ]]; then
+if [[ -z "${INPUT_PATH}.${LANGUAGE}" ]]; then
   echo "Input file nonexistent!" && exit 1
+fi
+if [[ -d "${WORK_DIR}" ]]; then
+  echo "Work directory must be nonexistent." && exit 1
 fi
 
 # Make operations visible to user.
 set -o xtrace
 
+mkdir -p "${WORK_DIR}"
+
+split \
+  --suffix-length=7 \
+  --additional-suffix=".split" \
+  --lines=1000 \
+  "${INPUT_PATH}.${LANGUAGE}" \
+  "${WORK_DIR}/tmp-"
+
 java \
   -mx3g \
   edu.stanford.nlp.pipeline.StanfordCoreNLP \
-  -file "${INPUT_PATH}" \
+  -file "${WORK_DIR}" \
+  -extension ".split" \
   -outputExtension ".${EXTENSION}" \
   -outputFormat "conll" \
-  -outputDirectory "$(dirname ${INPUT_PATH})" \
+  -outputDirectory "${WORK_DIR}" \
   -ssplit.eolonly true \
   -output.prettyPrint false \
   -output.columns word \
   -annotators tokenize,ssplit \
   -tokenize.language ar \
   -segment.model "${MODEL}" \
+  -replaceExtension true \
   || exit 1
 
-sed -i 's/\./\n/g' "${INPUT_PATH}.${EXTENSION}"
+FINAL="${WORK_DIR}/$(basename ${INPUT_PATH}).${EXTENSION}.${LANGUAGE}"
 
+cat ${WORK_DIR}/tmp-*.${EXTENSION} > "${FINAL}"
+
+sed -i 's/\./\n/g' "${FINAL}"
+
+cp "${FINAL}" "$(dirname ${INPUT_PATH})"
