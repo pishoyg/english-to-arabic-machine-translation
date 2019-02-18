e4e4ed1e1a8d2d9e9f861d5ec858281012339f7e
bishoyboshra <bishoyboshra95@gmail.com>
2019-02-18 15:25:13 +0200

    Improvments to setup script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 # Go to home directory.
 cd
 
+# Flags.
+while [ $# -gt 0 ]; do
+  case "${1}" in
+    --git_user_email=*)
+      GIT_USER_EMAIL="${1#*=}"
+      ;;
+    --git_user_name=*)
+      GIT_USER_NAME="${1#*=}"
+      ;;
+    *)
+      echo "Unknown flag: ${1}" && exit 1
+  esac
+  shift
+done
+
+# Make operations visible to user.
+set -o xtrace
+
 # email_specs.json.
 if [[ ! -f "${HOME}/email_specs.json" ]]; then
   echo "Please create email specs!" && exit 1
@@ -16,6 +34,14 @@ if [[ ! -d "${HOME}/english-to-arabic-machine-translation" ]]; then
   git clone https://github.com/bishoyboshra/english-to-arabic-machine-translation.git
 fi
 
+# GitHub account (optional).
+if [[ ! -z ${GIT_USER_EMAIL} ]]; then
+  git config --global user.email ${GIT_USER_EMAIL}
+fi
+if [[ ! -z ${GIT_USER_NAME} ]]; then
+  git config --global user.name ${GIT_USER_NAME}
+fi
+
 # nmt.
 if [[ ! -d "${HOME}/nmt" ]]; then
   git clone https://github.com/tensorflow/nmt.git
@@ -25,11 +51,3 @@ NMT_INIT="${HOME}/nmt/__init__.py"
 if [[ ! -f  "${NMT_INIT}" ]]; then
   touch "${NMT_INIT}"
 fi
-
-# Disable power-saving options, to keep the internet working.
-echo "TODO:
-su - mluser
-sudo iwconfig wlp3s0 power off
-sudo systemctl stop NetworkManager.service
-sudo systemctl disable NetworkManager.service
-exit"
---
+++ sudo_setup.sh
@@ -0,0 +1,11 @@
+set -o xtract
+
+# TODO: Install pip.
+# TODO: Install tf-nightly.
+# TODO: Install git.
+
+# Disable power-saving options, to keep the internet working (optional).
+echo "TODO:
+sudo iwconfig wlp3s0 power off
+sudo systemctl stop NetworkManager.service
+sudo systemctl disable NetworkManager.service"
