7c3db09fb2a9ddfa685fca78251cec0103baa5d4
Bishoy Boshra Labib Girgis <bishoyboshra95@gmail.com>
2019-02-22 18:50:08 +0200

    Update sudo_setup.sh

--- sudo_setup.sh
+++ sudo_setup.sh
@@ -1,6 +1,7 @@
 set -o xtrace
 
-sudo apt-get install default-jre
-sudo apt install python-pip python3-pip
+sudo apt install python3-pip
 sudo apt install git
+# python-pip
 # sudo apt install cuda-9.0
+# sudo apt-get install default-jre
