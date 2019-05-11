eb240a6f4e90ebbc9be4d4a9cc9c48175152c03c
bishoyboshra <bishoyboshra95@gmail.com>
2019-05-10 20:11:51 -0400

    Improvements to detokenization script.

--- detokenize.py
+++ detokenize.py
@@ -39,26 +39,31 @@ def TrieInsert(tok, detok):
 
 
 words = None
+memo = None
 def process(idx, strict=True):
-  if idx == len(words):
-    return True, []
+  if idx in memo and (strict or memo[idx] is not None):
+    return memo[idx]
+  ans = None
   node = trie
   attempts = list()
-  start = idx
-  while idx < len(words) and words[idx] in node[0]:
-    node = node[0][words[idx]]
-    idx += 1
+  next = idx
+  while next < len(words) and words[next] in node[0]:
+    node = node[0][words[next]]
+    next += 1
     if node[1]:
-      attempts.append((node[1], idx))
+      attempts.append((node[1], next))
   # Reverse attempts to consider the longest attempt first, according to the maximum-munch principle.
   attempts = reversed(attempts)
-  for buildup, idx in attempts:
-    success, sentence = process(idx, strict)
-    if success:
-      return True, [buildup] + sentence
-  if not strict:
-    return True, words[start] + process(start + 1, strict)
-  return False, None
+  for buildup, next in attempts:
+    process(next, strict)
+    if memo[next] is not None:
+      ans = [buildup] + memo[next]
+      break
+  if ans is None and not strict:
+    process(idx + 1, strict)
+    assert memo[idx + 1] is not None
+    ans = [words[idx]] + memo[idx + 1]
+  memo[idx] = ans
 
 
 def main():
@@ -69,16 +74,20 @@ def main():
       detok = detok.strip()
       TrieInsert(tok, detok)
   global words
+  global memo
   with open(args.detok, 'w') as detok:
     with open(args.tok) as tok:
-      for line in tok:
+      for i, line in enumerate(tok):
         words = line.split()
-        success, sentence = process(0)
-        if not success:
-          success, sentence = process(0, False)
-          assert success
-        detok.write(' '.join(sentence) + '\n')
-        print(' '.join(sentence) + '\n')
+        memo = {len(words): []}
+        process(0)
+        if memo[0] is None:
+          process(0, False)
+          assert memo[0] is not None
+        final = ' '.join(memo[0])
+        detok.write(final + '\n')
+        print(i)
+
 
 if __name__ == '__main__':
   main()
